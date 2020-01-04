a12bd089e6d3df166ef16ac6d344b3a2135eca64
Grace <mgracebodur@gmail.com>
2020-01-04 14:06:47 -0500

    Query paginate items working

--- src/drills.js
+++ src/drills.js
@@ -12,6 +12,7 @@ const knexInstance = knex({
 // })
 
 //1. Get all items that contain text
+//A function that takes one parameter for searchTerm which will be any string
 //function will query the shopping_list table using 
 //Knex methods and select the rows which have a name 
 //that contains the searchTerm using a case insensitive match.
@@ -29,3 +30,23 @@ function searchByShoppingListName(searchTerm) {
   }
   
   searchByShoppingListName('burger')
+
+//2. Get all items paginated
+//A function that takes one parameter for pageNumber which will be a number
+//The function will query the shopping_list table using Knex methods 
+//and select the pageNumber page of rows paginated to 6 items per page.
+
+function paginateShoppingListItems(page) {
+    const pageNumber = 6
+    const offset = pageNumber * (page - 1)
+    knexInstance
+      .select('id', 'name', 'price', 'date_added', 'checked','category')
+      .from('shopping_list')
+      .limit(pageNumber)
+      .offset(offset)
+      .then(result => {
+        console.log(result)
+      })
+}
+
+paginateShoppingListItems(1)

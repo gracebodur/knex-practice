0f303713954aceb4f173660655349df5023d59ed
Grace <mgracebodur@gmail.com>
2020-01-04 00:44:01 -0500

    Unable to acquire a connection, need to fix .env

--- src/practice.js
+++ src/practice.js
@@ -5,14 +5,19 @@ const knexInstance = knex({
     connection: process.env.DB_URL
 })
 
-console.log('knex and driver installed correctly')
+// console.log('knex and driver installed correctly')
 
-const q1 = knexInstance('amazong_products').select('*').toQuery();
-const q2 = knexInstance.from('amazong_products').select('*').toQuery();
+// const q1 = knexInstance('amazong_products').select('*').toQuery();
+// const q2 = knexInstance.from('amazong_products').select('*').toQuery();
 
-console.log('q1:', q1)
+// console.log('q1:', q1)
 
-console.log('q2:', q2)
+// console.log('q2:', q2)
+
+knexInstance('amazong_products').select('*')
+    .then(result => {
+        console.log(result)
+    })
 
 
   

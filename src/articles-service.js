39679701259eceb3c23e155737995011e9e767ba
Grace <mgracebodur@gmail.com>
2020-01-08 01:36:57 -0500

    All articles service object test pass.

--- src/articles-service.js
+++ src/articles-service.js
@@ -3,13 +3,34 @@
 //The first method is for getting all articles
 const ArticlesService = {
     getAllArticles(knex) {
-        // return 'all the articles!!'
-        // return Promise.resolve('all the articles!!')
-        return knex.select('*').from('blogful_articles')
+        return knex
+        .select('*')
+        .from('blogful_articles')
     },
-    insertArticle() {
-        return Promise.resolve({})
+    insertArticle(knex, newArticle) {
+        return knex
+            .insert(newArticle)
+            .into('blogful_articles')
+            .returning('*')
+            .then(rows => rows[0])
+        },
+    getById(knex, id) {
+        return knex
+            .from('blogful_articles')
+            .select('*')
+            .where('id', id)
+            .first()
+        },
+    deleteArticle(knex, id) {
+        return knex('blogful_articles')
+            .where({ id })
+            .delete()
     },
+    updateArticle(knex, id, newArticleFields) {
+        return knex('blogful_articles')
+        .where({ id })
+        .update(newArticleFields)
+    }
 }
 
 
--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -31,12 +31,8 @@ describe(`Articles service object`, function() {
         })
     })
     
-    after(() => db.destroy())
-
     before(() => db('blogful_articles').truncate())
-
     afterEach(() => db('blogful_articles').truncate())
-
     after(() => db.destroy())
 
 // we can use separate describe blocks for when the database has data 
@@ -50,41 +46,91 @@ describe(`Articles service object`, function() {
 //     })
 
 // describe(`getAllArticles()`, () => {
-    context(`Given'blogful_articles has data`, () => {
-        // test that ArticlesService.getAllArticles gets data from table
-        before(() => {
-            return db
-                .into('blogful_articles')
-                .insert(testArticles)
-        })
+context(`Given'blogful_articles has data`, () => {
+    // test that ArticlesService.getAllArticles gets data from table
+    // before(() => {
+    beforeEach(() => {
+        return db
+            .into('blogful_articles')
+            .insert(testArticles)
+    })
 
-        it(`getAllArticles() resolves all articles from 'blogful_articles' table`, () => {
-            return ArticlesService.getAllArticles(db)
-            .then(actual => {
-                expect(actual).to.eql(testArticles.map(article => ({
-                    ...article,
-                    date_published: new Date(article.date_published)
-                    // expect(actual).to.eql(testArticles)
-            })))
+    it(`getAllArticles() resolves all articles from 'blogful_articles' table`, () => {
+        return ArticlesService.getAllArticles(db)
+        .then(actual => {
+            expect(actual).to.eql(testArticles)
         })
     })
 
-    context(`Given 'blogful_articles' has no data`, () => {
-        it(`getAllArticles() resolves an empty array`, () => {
-            return ArticlesService.getAllArticles(db)
+    it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
+        const thirdId = 3
+        const thirdTestArticle = testArticles[thirdId - 1]
+        return ArticlesService.getById(db, thirdId)
             .then(actual => {
-                expect(actual).to.eql([])
+                expect(actual).to.eql({
+                    id: thirdId,
+                    title: thirdTestArticle.title,
+                    content: thirdTestArticle.content,
+                    date_published: thirdTestArticle.date_published,
+                })
             })
         })
 
-        it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
-            const newArticle ={
-                title: 'Test new title',
-                content: 'Test new content',
-                date_published: new Date('2020-01-01T00:00:00.000Z'),
-            }
-            return ArticlesService.insertArticle(db, newArticle)
+    it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+        const articleId = 3
+        return ArticlesService.deleteArticle(db, articleId)
+        .then(() => ArticlesService.getAllArticles(db))
+        .then(allArticles => {
+        // copy the test articles array without the "deleted" article
+        const expected = testArticles.filter(article => article.id !== articleId)
+        expect(allArticles).to.eql(expected)
         })
     })
-})
+
+    it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+        const idOfArticleToUpdate = 3
+        const newArticleData = {
+            title: 'updated title',
+            content: 'updated content',
+            date_published: new Date(),
+        }
+        return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+            .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
+            .then(article => {
+                expect(article).to.eql({
+                   id: idOfArticleToUpdate,
+                   ...newArticleData,
+                })
+            })
+        })
+    })
+
+context(`Given 'blogful_articles' has no data`, () => {
+    beforeEach(() => {
+        return db('blogful_articles').truncate()
+    })
+    
+    it(`getAllArticles() resolves an empty array`, () => {
+        return ArticlesService.getAllArticles(db)
+        .then(actual => {
+            expect(actual).to.eql([])
+        })
+    })
+
+    it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
+        const newArticle ={
+            title: 'Test new title',
+            content: 'Test new content',
+            date_published: new Date('2020-01-01T00:00:00.000Z'),
+        }
+        return ArticlesService.insertArticle(db, newArticle).then(actual => {
+            expect(actual).to.eql({
+                id: 1,
+                title: newArticle.title,
+                content: newArticle.content,
+                date_published: newArticle.date_published,
+                })
+            })
+        })
+    })
 })
